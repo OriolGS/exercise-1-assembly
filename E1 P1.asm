1998b2afae85358f1ce4af31c5ca0684eacb92b9
OriolGS <oriol.gs.dam@gmail.com>
2025-11-12 17:57:56 +0100

    Bug se abren los mismos

--- E1 P1.asm
+++ E1 P1.asm
@@ -42,25 +42,33 @@ Inicio	bsf	STATUS,RP0	        ;Selecciona Banco 1
 				clrf	ANSEL	;Puerta A digital
 				clrf	ANSELH	;Puerta B digital
 			bcf		STATUS,RP1	;Selecciona Banco 1
-			movlw	b'00111111'		
+			movlw	b'00000010'		
 			movwf	TRISA		;Puerta A se configura como entrada
 			movlw	b'00000000'		
 			movwf	TRISB		;Puerta B se configura como salida
+			movlw	b'00000000'		
+			movwf	TRISC		;Puerta C se configura
+			movlw	b'00011110'		
+			movwf	TRISD		;Puerta D se configura
 		bcf		STATUS,RP0		;Selecciona Banco 0
 
 ApagarTodo	
 		clrf	PORTB	;Apagar los LEDs conectados al PORTB
+		bcf		PORTC,2
+		bcf		PORTD,5
+		bcf		PORTD,6
+		bcf		PORTD,7
+		
 	
 InterE3				 
-	btfss PORTA,RA1	;Leer la entrada E3 (RA1)
+	btfss PORTA,RA1		;Leer la entrada E3 (RA1)
 	goto E3Apagado		;Si está apagado (RA1==0) apagar todos los LEDs
 	goto E3Encendido	;Si está encendido (RA1==1) encendres LED's (S3-S0) 
 						;   mantener estado LED's (S7-S4)
 						;   y mirar pulsadores.
 
 E3Apagado
-			;ApagarTodo
-		goto ApagarTodo	
+		goto ApagarTodo	;ApagarTodo
 
 ;BUCLE PRINCIPAL
 E3Encendido			
@@ -69,18 +77,18 @@ E3Encendido
 	goto MirarPulsadorE7	
 	
 MirarPulsadorE7			
-	btfsc PORTA,5			;Leer pulsador E7 (RA5)
+	btfsc PORTD,1			;Leer pulsador E7 (RA5)
 	goto MirarPulsadorE6 	
 	goto PulsadorE7Activo
 	
 MirarPulsadorE6			
-	btfsc PORTA,4
+	btfsc PORTD,2
 	goto MirarPulsadorE5 	
 	goto PulsadorE6Activo			;Si está activo (RA4==0)
 					;ir a activar LEDs S7-S4 (RB7-RB4='0100')	
 		
 MirarPulsadorE5
-	btfsc PORTA,3
+	btfsc PORTD,3
 	goto MirarPulsadorE4 	
 	goto PulsadorE5Activo			
 					;Leer pulsador E5 (RA3)
@@ -89,7 +97,7 @@ MirarPulsadorE5
 					;ir a activar LEDs S7-S4 (RB7-RB4='0010')	
 		
 MirarPulsadorE4
-	btfsc PORTA,2
+	btfsc PORTD,4
 	goto NoActivos 	
 	goto PulsadorE4Activo		
 					;Leer pulsador E4 (RA2)
@@ -104,33 +112,33 @@ NoActivos
 		goto	InterE3
 
 PulsadorE7Activo
-        bsf PORTB,7         
-        bcf PORTB,6
-		bcf PORTB,5
-		bcf PORTB,4
+        bsf PORTD,5         
+        bcf PORTC,2
+		bcf PORTD,7
+		bcf PORTD,6
 		goto InterE3
 		
 PulsadorE6Activo
-        bcf PORTB,7         
-        bsf PORTB,6
-		bcf PORTB,5
-		bcf PORTB,4            ;Activar LEDs S7-S4 (RB7-RB4='1000')
+        bcf PORTD,5         
+        bsf PORTC,2
+		bcf PORTD,7
+		bcf PORTD,6            ;Activar LEDs S7-S4 (RB7-RB4='1000')
 		
 		goto InterE3
 		
 PulsadorE5Activo
-        bcf PORTB,7         
-        bcf PORTB,6
-		bsf PORTB,5
-		bcf PORTB,4            ;Activar LEDs S7-S4 (RB7-RB4='0010')	
+        bcf PORTD,5         
+        bsf PORTC,2
+		bcf PORTD,7
+		bcf PORTD,6            ;Activar LEDs S7-S4 (RB7-RB4='0010')	
 		
 		goto	InterE3		
 
 PulsadorE4Activo
-        bcf PORTB,7         
-        bcf PORTB,6
-		bcf PORTB,5
-		bsf PORTB,4            ;Activar LEDs S7-S4 (RB7-RB4='0001')	
+        bcf PORTD,5         
+        bsf PORTC,2
+		bcf PORTD,7
+		bcf PORTD,6            ;Activar LEDs S7-S4 (RB7-RB4='0001')	
 		
 		goto	InterE3	
 		
